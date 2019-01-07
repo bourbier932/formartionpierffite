5fa53234b1cd1c11aca128608d21042b29700237
bourbier932 <45196838+bourbier932@users.noreply.github.com>
2019-01-07 16:54:16 +0100

    js

---
+++ Javascript/js/concatenation.js
@@ -0,0 +1,13 @@
+var tomtom = "225";
+
+var toto = true;
+
+var yancy = 1.09;
+
+calcul = tomtom + ' ' + toto + ' '+ yancy;opvg
+
+console.log(calcul);
+document.write(calcul + "<br>");n 
+
+
+document.write("amir")
